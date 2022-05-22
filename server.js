936f60d5ab5870e2232cb67ddd1dd619942bb7ec
gordonlc18 <gordonlc18@gmail.com>
2022-05-22 13:38:23 -0400

    Add Inquirer prompt

--- server.js
+++ server.js
@@ -36,3 +36,76 @@ const figlet = require('figlet');
       //   Main Function
  eTracker();
 });
+
+// Inquirer prompt
+const eTracker = () => {
+    inquirer.prompt({
+        name: 'action',
+        type: 'list',
+        message: 'Please select a task?',
+        choices: [
+            'View All Departments',
+            'View All Employees',
+            'View All Roles',
+            'Update Employee Roles',
+            'Add a Department',
+            'Add an Employee',
+            'Add a Role',
+            'Delete an Employee',
+            'Delete a Department',
+            'Delete a Role',
+            "Exit",
+        ],
+    })
+    .then((answer) => {
+        switch (answer.action) {
+            case 'View a Department':
+                viewDepartments();
+                break;
+  
+            case 'View Employees':
+                viewEmployees();
+                break;
+  
+                case 'View a Role':
+                    viewRoles();
+                    break;
+
+                case 'Update Employee Roles':
+                updateRole();
+                break;
+
+            case 'Add a Department':
+                addDepartment();
+                break;
+  
+                case 'Add an Employee':
+                addEmployee();
+                break;
+  
+                case 'Add a Role':
+                    addRole();
+                    break;
+  
+                case 'Delete an Employee':
+                    deleteEmployee();
+                    break;
+  
+            case 'Delete a Department':
+               deleteDepartment();
+                break;
+  
+                case 'Delete a Role':
+                deleteRole();
+                break;
+  
+            case 'Exit':
+                exit();
+                break;
+  
+            default:
+                console.log(`Invalid action: ${answer.action}`);
+                break;
+        }
+    });
+  };
